e4d96949dbc5216bbc9d631c0248b8642cbe37b0
venusCh <ec2-user@ip-172-31-23-201.us-west-2.compute.internal>
2016-02-25 01:27:59 +0000

    changed migration again

--- db/migrate/20160220011155_change_column_name.rb
+++ db/migrate/20160220011155_change_column_name.rb
@@ -1,5 +1,5 @@
 class ChangeColumnName < ActiveRecord::Migration
   def change
-	rename_column :asks, :from, 'integer USING CAST("column_to_change" AS integer)'
+	rename_column :asks, :from, 'integer USING CAST("user_id" AS integer)'
   end
 end
