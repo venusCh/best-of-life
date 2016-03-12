ab6c8a7811ff72b705b1034544094ee815de55a5
Venus Chintala <venusiimc@gmail.com>
2016-03-11 23:39:49 -0500

    fixed the issue with accept display

--- app/controllers/asks_controller.rb
+++ app/controllers/asks_controller.rb
@@ -3,6 +3,12 @@ class AsksController < ApplicationController
 	def create
 		@giving = Giving.find(params[:giving_id])
 		@ask = @giving.asks.create(ask_params)
+
+		if current_user == nil
+			sign_in_and_redirect @giving
+			return
+		end
+
 		@ask.user_id = current_user.id
 		@ask.save
 		redirect_to @giving
