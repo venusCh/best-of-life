aa2bea5f37adfab9605bd390cccc5eb10759d769
Venus Chintala <venusiimc@gmail.com>
2016-03-22 22:09:17 -0400

    added mail_email for send email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  has_many :givings
-  acts_as_messageable
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
+    		:recoverable, :rememberable, :trackable, :validatable
+	has_many :givings
+	acts_as_messageable
 
 	def mailboxer_email(object)
 		return self.email
+	end
+
+	def mail_email(object)
+		return email
 	end
 
 end
