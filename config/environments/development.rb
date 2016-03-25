e8db137ea00647ee60b41240304679a2739e4853
Venus Chintala <venusiimc@gmail.com>
2016-03-25 17:29:21 -0400

    aws s3 config for paperclip updated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,10 +40,13 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # :s3_host_name => 'REMOVE_THIS_LINE_IF_UNNECESSARY',
   config.paperclip_defaults = {
     :storage => :s3,
-    :bucket => 'giversappstorage'
+    :s3_credentials => {
+      :bucket => ENV['giversappstorage'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
 
 end
