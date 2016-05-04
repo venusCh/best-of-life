b2f4060e583176da16dd9823afba55e8e14e1426
Venus Chintala <venusiimc@gmail.com>
2016-05-03 22:00:23 -0400

    updated environment.rb for email settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,16 +14,6 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   config.action_mailer.perform_deliveries = true
-  
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'givers-app.org',
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
