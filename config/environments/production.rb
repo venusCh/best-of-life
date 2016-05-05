0c5b8b007172d6499c77481ca5b8342f20121e69
Venus Chintala <venusiimc@gmail.com>
2016-05-04 22:26:16 -0400

    updated prod environment file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.smtp_settings = {
+        :address              => "smtp.gmail.com",
+        :port                 => 587,
+        :domain               => "givers-app.org",
+        :user_name            => ENV['GMAIL_USERNAME'],
+        :password             => ENV['GMAIL_PASSWORD'],
+        :authentication       => :plain,
+        :enable_starttls_auto => true
+  }
+
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'http://giversapp-env.at2tevh5f9.us-west-2.elasticbeanstalk.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
