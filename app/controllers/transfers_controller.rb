1f1b5e5e5e1d5127a178adb993b88c93ff9babb9
Venus Chintala <venusiimc@gmail.com>
2016-05-07 21:38:12 -0400

    added checks to avoid duplicate giving

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -5,19 +5,27 @@ class TransfersController < ApplicationController
 		@giving = Giving.find(params[:giving_id])
 		@transfer = @giving.transfers.create(transfer_params)
 
-		@transfer.from = current_user.id
-		@transfer.to = params[:transfer][:recipient]
-		@transfer.conversation = params[:transfer][:conversation]
+		@check = Transfer.find_by_from_and_to_and_conversation(current_user.id, 
+																params[:transfer][:recipient],
+																params[:transfer][:conversation])
 
-		@transfer.save
+		if @check.nil? then
+			@transfer.from = current_user.id
+			@transfer.to = params[:transfer][:recipient]
+			@transfer.conversation = params[:transfer][:conversation]
 
-		@giving.status = 1 # In-use
-		@giving.save
+			@transfer.save
 
-		@recipient = User.find(@transfer.to)
-	    UserMailer.send_accept_notification(@recipient, current_user, @giving).deliver_now
+			@giving.status = 1 # In-use
+			@giving.save
 
-		redirect_to :back
+			@recipient = User.find(@transfer.to)
+		    UserMailer.send_accept_notification(@recipient, current_user, @giving).deliver_now
+
+			redirect_to :back
+		else
+			redirect_to :back, notice: "This request is already accepted."
+		end
 	end
 
 	private
