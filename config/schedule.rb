5f06dfc49d5ae0a70f9bcf0da43a468256107b11
Venus Chintala <venusiimc@gmail.com>
2016-05-12 00:53:39 -0400

    changed schedule to twice a day

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 case @environment
 when 'production'
-	every 1.day, :at => '5:00 pm' do
+	every "0 6,18 * * *" do
 		runner "Transfer.send_notifications", output: {:error => "#{path}/log/error.log", :standard => "#{path}/log/cron.log"}
 	end
 end
