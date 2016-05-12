e473866abc620f22a1c2cc887d89c55b74ea0258
Venus Chintala <venusiimc@gmail.com>
2016-05-12 00:43:31 -0400

    updated schedule to daily, not to spam the error log

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 case @environment
 when 'production'
-	every 2.minutes do
+	every 1.day :at => '5:00 pm' do
 		runner "Transfer.send_notifications", output: {:error => "#{path}/log/error.log", :standard => "#{path}/log/cron.log"}
 	end
 end
