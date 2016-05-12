337ebbd394663516b5b40ecc5273392bb6496b91
Venus Chintala <venusiimc@gmail.com>
2016-05-12 00:31:08 -0400

    changed to double quotes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 case @environment
 when 'production'
 	every 2.minutes do
-		runner "Transfer.send_notifications", output: {:error => '#{path}/log/error.log', :standard => '#{path}/log/cron.log'}
+		runner "Transfer.send_notifications", output: {:error => "#{path}/log/error.log", :standard => "#{path}/log/cron.log"}
 	end
-end
+end"
