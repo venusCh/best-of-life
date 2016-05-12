a482e7ad47062f4adc0d8e41b612404f2171b416
Venus Chintala <venusiimc@gmail.com>
2016-05-11 21:29:22 -0400

    updated schedule.rb for prod environemt

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,19 +1,6 @@
-#set :output, "#{path}/log/cron.log"
-
-#every :day, :at => '5:00pm' do
-#	rake "send_digest_email"
-#end
-
-every 2.minutes do
-	runner "Transfer.send_notifications", output: {:error => '#{path}/log/error.log', :standard => '#{path}/log/cron.log'}
+case @environment
+when 'production'
+	every 2.minutes do
+		runner "Transfer.send_notifications", output: {:error => '#{path}/log/error.log', :standard => '#{path}/log/cron.log'}
+	end
 end
-
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
