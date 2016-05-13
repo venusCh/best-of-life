7c3a725a6558ca0013797759460a3d0248dcf1c7
Venus Chintala <venusiimc@gmail.com>
2016-05-13 19:31:30 -0400

    added debug statements

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,7 @@ case @environment
 when 'production'
 	#every "0 6,18 * * *" do
 	every 2.minutes do
+		puts "\n\nabout to call Transfer.send_notifications...\n"
 		runner "Transfer.send_notifications" #, output: {:error => "#{path}/log/error.log", :standard => "#{path}/log/cron.log"}
 	end
 end
