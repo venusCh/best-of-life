56c702a14ff7d5d0d65abde5ea307a42d73312de
Venus Chintala <venusiimc@gmail.com>
2016-05-13 20:17:48 -0400

    Finally updated schedule to twice a day

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,7 @@
 case @environment
 when 'production'
-	#every "0 6,18 * * *" do
-	every 2.minutes do
+	every "0 6,18 * * *" do
 		puts "\n\ncalling Transfer.send_notifications...\n"
-		command "/bin/bash -l -c 'cd /var/app/current && rails runner -e production \"Transfer.send_notifications\"'"
+		command 'cd /var/app/current && rails runner -e production \"Transfer.send_notifications\"'
 	end
 end
