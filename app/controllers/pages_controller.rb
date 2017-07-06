96eac2649e9f78f0c77716abdc92131645dd29a3
Venus Chintala <venusiimc@gmail.com>
2017-07-05 21:19:05 -0400

    updated cert for giversapp.org

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
 	def letsencrypt
-		render text: "FIIcr93HLtADs8vkA44mAqfqySTE9o9QWtKimU7Nn1g.ewehnbdEmG4pWeejekVHgD0nQvXYtZOcRvQJcq49Erc"
+		render text: "MKbEniv8nyhaqJkBP7EGPvrJDyjWya3vJF9-2ElunX4.ewehnbdEmG4pWeejekVHgD0nQvXYtZOcRvQJcq49Erc"
 	end
 end
